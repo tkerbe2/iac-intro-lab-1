e2cfeec992afbb8f4bc22c7b66daad853671a41d
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-06-29 17:30:15 -0500

    Pushing video changes

--- vnet.tf
+++ vnet.tf
@@ -4,7 +4,7 @@
 
 
 resource "azurerm_resource_group" "rg_main" {
-  name     = "example"
+  name     = "${var.org}-${var.region}-rg"
   # Notice I used a variable below by using var.<variable name>
   location = var.region
 }
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "rg_main" {
 # Below is where I define my Virtual Network VNet resource
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "${var.org}-${var.region}-vnet"
   location            = azurerm_resource_group.rg_main.location
   resource_group_name = azurerm_resource_group.rg_main.name
   address_space       = ["10.0.0.0/16"]
